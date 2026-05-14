a8c82f126909f65e59700701ebaa91e567af56dc
Aya Farag <ayasaid94.as@gmail.com>
2026-05-14 22:10:44 +0300

    update sevice and interface

--- HIS.Application/Interface/IPatientService.cs
+++ HIS.Application/Interface/IPatientService.cs
@@ -1,4 +1,5 @@
 ﻿using HIS.Application.DTO;
+using HIS.Domain.Aggregates.PatientAggregate.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,37 @@ namespace HIS.Application.Interface
 {
     public interface IPatientService
     {
-        Task AddPatientAsync(PatientDto patientDto);
-        Task<PatientDto> GetPatientByIdAsync(Guid id);
-        Task<List<PatientDto>> GetAllPatientsAsync();
-        Task UpdatePatientAsync(PatientDto patientDto);
-        Task DeletePatientAsync(Guid id);
+        //Task AddPatientAsync(PatientDto patientDto);
+        //Task<PatientDto> GetPatientByIdAsync(Guid id);
+        //Task<List<PatientDto>> GetAllPatientsAsync();
+        //Task UpdatePatientAsync(PatientDto patientDto);
+        //Task DeletePatientAsync(Guid id);
+
+        // ----------------------------------------
+
+        //1
+        Task AddAsync(PatientDto patient);
+
+        //2
+        Task<PatientDto?> GetByIdAsync(Guid patientId);
+
+        Task<PatientDto?> GetByMRNAsync(string mrn);
+
+        Task<PatientDto?> GetByNationalIdentityAsync(string nationalIdentity);
+
+        Task<bool> ExistsByNationalIdentityAsync(string nationalIdentity);
+
+        Task<bool> ExistsDuplicateActivePatientAsync(string nationalIdentity, string phoneNumber);
+
+        Task<bool> HasActiveAppointmentsAsync(Guid patientId);
+
+        //3
+        void Update(Patient patient);
+
+        //4 upload medical record function
+        //5 get patient documents function
+        //6 add medical history function
+        //7 search patients by name, NID, MRN, phone)
+        //8 deactivate patient function
     }
 }
--- HIS.Application/Service/PatientService.cs
+++ HIS.Application/Service/PatientService.cs
@@ -1,5 +1,7 @@
 ﻿using HIS.Application.DTO;
 using HIS.Application.Interface;
+using HIS.Domain.Aggregates.PatientAggregate.Entities;
+using HIS.Domain.Interfaces.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,27 +12,53 @@ namespace HIS.Application.Service
 {
     public class PatientService : IPatientService
     {
-        public Task AddPatientAsync(PatientDto patientDto)
+        public IPatientRepository _patientRepository;
+        public PatientService(IPatientRepository patientRepository)
+        {
+            _patientRepository = patientRepository;
+        }
+
+        public Task AddAsync(PatientDto patient)
+        {
+            // automapper
+            // validation
+            // exception handling
+            // conditions
+            // call repository
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsByNationalIdentityAsync(string nationalIdentity)
         {
             throw new NotImplementedException();
         }
 
-        public Task DeletePatientAsync(Guid id)
+        public Task<bool> ExistsDuplicateActivePatientAsync(string nationalIdentity, string phoneNumber)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<PatientDto>> GetAllPatientsAsync()
+        public Task<PatientDto?> GetByIdAsync(Guid patientId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<PatientDto> GetPatientByIdAsync(Guid id)
+        public Task<PatientDto?> GetByMRNAsync(string mrn)
         {
             throw new NotImplementedException();
         }
 
-        public Task UpdatePatientAsync(PatientDto patientDto)
+        public Task<PatientDto?> GetByNationalIdentityAsync(string nationalIdentity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HasActiveAppointmentsAsync(Guid patientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Patient patient)
         {
             throw new NotImplementedException();
         }
